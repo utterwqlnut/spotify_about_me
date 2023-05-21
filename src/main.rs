b3670ddc1d9649ccb5175389b2e2e00db5829e41
Dhruva Chayapathy <dhruva.chayapathy@gmail.com>
2023-05-20 22:27:23 -0400

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
 use crate::grab::*;
 use std::fs;
+use std::io;
 use std::process::Command;
 use minijinja::{Environment, context};
 pub mod grab;
 
 fn main () {
-    let result = grab::get_user_data(true);
+    println!("Would you like a long or short about me (1 for long 2 for short)");
+
+    let mut which = String::new();
+
+    io::stdin().read_line(&mut which).expect("Failed to read line");
+
+    let which: u32 = which.trim().parse().expect("Please type a number");
+    
+    let result;
+
+    if which == 1 as u32 {
+        result = grab::get_user_data(true);
+    } else {
+        result = grab::get_user_data(false);
+    }
+
     let mut env = Environment::new();
-    env.add_template("template",include_str!("template.html")).unwrap();
-    let tmpl = env.get_template("template").unwrap();
 
     match result {
         Grabber::Long{name, pfp, followers, top3artists, top3songs, last_played} => {
+        env.add_template("template",include_str!("template.html")).unwrap();
+
+        let tmpl = env.get_template("template").unwrap();
             fs::write("./about_me.html", tmpl.render(context!(user => name.unwrap(), img => pfp.unwrap(), 
                                                 recent => last_played.unwrap(), artists => top3artists.unwrap(),
                                                 songs => top3songs.unwrap(), followers => followers.unwrap()))
                       .unwrap()).expect("issue writing to file");
 
         },
-        Grabber::Short{..} => println!("nah"),
+        Grabber::Short{name, pfp, followers} => {
+            env.add_template("template",include_str!("template2.html")).unwrap();
+            let tmpl = env.get_template("template").unwrap();
+
+            fs::write("./about_me.html", tmpl.render(context!(user => name.unwrap(), img => pfp.unwrap(), 
+                                                              followers => followers.unwrap()))
+                      .unwrap()).expect("issues writing to file");
+        },
     }
+
     let _command = Command::new("zsh").arg("-c").arg("open about_me.html").spawn().expect("issue opening file");
+
 }
